23a9a6581263529ae4a4b0677a1a30612d341152
Arron\Arron1992 <codewarrior1992@gmail.com>
2022-10-21 11:21:27 +0800

    transition

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from "vue";
+import TransitionDemo from "./components/TransitionDemo.vue";
+const toggle = ref(false);
+</script>
+<template>
+  <section>
+    <button
+      type="button"
+      class="container buttonOne"
+      @click="toggle = !toggle"
+      :class="{ active: toggle }"
+    >
+      <div class="top">我是標題</div>
+      <div class="bottom">
+        <div class="content">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat
+          animi ea quod quos perferendis, odio commodi, dolorem amet dignissimos
+          dicta suscipit? Fugit, explicabo eius autem libero perferendis dolorem
+          eos deleniti.
+        </div>
+      </div>
+    </button>
+
+    <TransitionDemo />
+  </section>
+</template>
---
+++ src/components/TransitionDemo.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from "vue";
+const toggle = ref(false);
+</script>
+<template>
+  <section>
+    <button type="button" class="container buttonTwo" @click="toggle = !toggle">
+      <div class="top">我是標題</div>
+      <Transition name="slide">
+        <div class="bottom" v-if="toggle">
+          <div class="content">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat
+            animi ea quod quos perferendis, odio commodi, dolorem amet
+            dignissimos dicta suscipit? Fugit, explicabo eius autem libero
+            perferendis dolorem eos deleniti.
+          </div>
+        </div>
+      </Transition>
+    </button>
+  </section>
+</template>
